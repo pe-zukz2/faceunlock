863e45e4cc596dc1f25b978f91786f3b7f1736de
LibXZR <xzr467706992@163.com>
2020-07-15 09:18:12 +0800

    script: Print out what is applying

--- patch.sh
+++ patch.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 
+echo "Applying packages/apps/Settings"
 cd packages/apps/Settings
 git apply ${DIR}/packages_apps_settings.diff --verbose
 cd ../../..
 
+echo "Applying framework/base"
 cd frameworks/base
 git apply ${DIR}/framework_base.diff --verbose
 cd ../..
 
+echo "Applying vendor/aosp"
 cd vendor/aosp
 git apply ${DIR}/vendor_aosp.diff --verbose
 cd ../..
